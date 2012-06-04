22ae367590228bbc2d1bb723e19673efc48af035
pitouli@gmail.com <pitouli@gmail.com@f75cda36-b3ad-3264-fc5e-22c7ee9b6161>
2012-06-04 23:15:24 +0000

    + L'interface de la calculatrice a été complétement revue. Toutes les fonctions sont accessibles par des boutons, qui se verront attribués des raccorucis. Les boutons sont organisé dans différents onglets. Préparation de la gestion d'onglets de piles.

--- trunk/ProjetLO21/pile.h
+++ trunk/ProjetLO21/pile.h
@@ -8,13 +8,21 @@ class Pile : public QList<expression::Expression*>
 {
 public:
     Pile();
-    void clear();
-    void swap(expression::Expression* i, expression::Expression* j);
-    void dup();
+
+    static Pile* get_curPile() { return _curPile; } // Retourne le pointeur vers la pile courante
+    static void set_curPile(Pile* newCurPile) { _curPile = newCurPile; } // Change la pile courante
+
     void push(expression::Expression* const expr) { this->append(expr); }
     expression::Expression* pop() { return this->takeLast(); }
-    void drop();
+
+    void clear();
+    void swap(expression::Expression* i, expression::Expression* j);
     void sum(expression::Expression* x);
+    void mean(expression::Expression* x);
+    void dup();
+    void drop();
+private:
+    static Pile* _curPile; // Stocke le pointeur vers la pile en cours d'utilisation
 };
 
 #endif // PILE_H
